53f9ca558b92429c7ab59e052693bef042f2c874
remy-cpu <3062857934@qq.com>
2021-02-24 09:59:05 +0800

    Enter

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import './style/index.less';
+// import Food from './modules/Food'
+// import ScorePanel from './modules/ScorePanel'
+
+
+// const scorePanel = new ScorePanel(100 ,2);
+// for (let i = 0;i < 200; i++) {
+//   scorePanel.addScore()
+// }
+
+import GameControl from './modules/GameControl';
+const gameControl = new GameControl();
+
+// setInterval(() => {
+//   console.log(gameControl.direction);
+// },1000)
